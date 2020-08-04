4588190e2dc6e2f4fa770df44b5e70cc17e10ad3
Aditya Gupta <slimshady9852@gmail.com>
2020-08-04 20:07:18 +0530

    dict_meaning t-3

--- app/src/main/java/com/google/codelab/mlkit/executeDict.java
+++ app/src/main/java/com/google/codelab/mlkit/executeDict.java
@@ -1,15 +1,40 @@
 package com.google.codelab.mlkit;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.util.Log;
 
+import androidx.annotation.RequiresApi;
+
+import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+import java.nio.file.Files;
 import java.util.*;
 import com.google.gson.*;
 import com.squareup.okhttp.*;
 
-public class executeDict extends AsyncTask<Void , Void, String> {
+import edmt.dev.edmtdevcognitivevision.Utils.Utils;
+
+import static com.google.codelab.mlkit.MainActivity.*;
+import static edmt.dev.edmtdevcognitivevision.Utils.Utils.EOF;
+
+public class executeDict extends AsyncTask<InputStream , Void, String> {
+
+    private static final int DEFAULT_BUFFER_SIZE = 1024 * 4;
+    public static final int EOF = -1;
+    private static final int STRING_BUILDER_SIZE = 256;
+    public static final String EMPTY = "";
 
 
     private static String subscriptionKey = "4ca7b53995e041cbab318cbde301835e";
@@ -21,7 +46,6 @@ public class executeDict extends AsyncTask<Void , Void, String> {
 
     public OkHttpClient client = new OkHttpClient();
 
-   
     public static String prettify(String json_text) {
         JsonParser parser = new JsonParser();
         JsonElement json = parser.parse(json_text);
@@ -29,12 +53,52 @@ public class executeDict extends AsyncTask<Void , Void, String> {
         return gson.toJson(json);
     }
 
+    public static int copy(final InputStream input, final OutputStream output) throws IOException {
+        final long count = copyLarge(input, output);
+        if (count > Integer.MAX_VALUE) {
+            return -1;
+        }
+        return (int) count;
+    }
+
+    public static long copy(final InputStream input, final OutputStream output, final int bufferSize)
+            throws IOException {
+        return copyLarge(input, output, new byte[bufferSize]);
+    }
+
+    public static long copyLarge(final InputStream input, final OutputStream output, final byte[] buffer)
+            throws IOException {
+        long count = 0;
+        int n;
+        while (EOF != (n = input.read(buffer))) {
+            output.write(buffer, 0, n);
+            count += n;
+        }
+        return count;
+    }
+
+    public static long copyLarge(final InputStream input, final OutputStream output)
+            throws IOException {
+        return copy(input, output, DEFAULT_BUFFER_SIZE);
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected String doInBackground(Void...voids) {
-        MediaType mediaType = MediaType.parse("application/json");
+    protected String doInBackground(InputStream...inputStreams) {
+        MediaType mediaType = MediaType.parse("application/octet-stream");
         Log.i("dict" , "2");
+        byte[] data;
+        try {
+            data = Utils.toByteArray(inputStreams[0]);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String sb = new String(data);
+
         RequestBody body = RequestBody.create(mediaType,
-                "{\"url\":\"" + imageToAnalyze + "\"}");
+                sb.toString());
         Log.i("dict" , "3");
         Request request = new Request.Builder()
                 .url(url).post(body)
--- app/src/main/java/com/google/codelab/mlkit/find_meaning.java
+++ app/src/main/java/com/google/codelab/mlkit/find_meaning.java
@@ -1,11 +1,13 @@
 package com.google.codelab.mlkit;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.google.gson.Gson;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -23,13 +28,27 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.util.concurrent.ExecutionException;
 
+import edmt.dev.edmtdevcognitivevision.Contract.AnalysisResult;
+import edmt.dev.edmtdevcognitivevision.Rest.VisionServiceException;
+import edmt.dev.edmtdevcognitivevision.VisionServiceClient;
+import edmt.dev.edmtdevcognitivevision.VisionServiceRestClient;
+
 public class find_meaning extends AppCompatActivity {
+
+    private static String subscriptionKey = "4ca7b53995e041cbab318cbde301835e";
+    private static String endpoint = "https://dictdetection.cognitiveservices.azure.com/";
+    String url = endpoint + "vision/v3.0/ocr?language=unk&detectOrientation=true";
+
+    private static final String imageToAnalyze =
+            "https://7esl.com/wp-content/uploads/2019/12/positive-words-2.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,8 +63,50 @@ public class find_meaning extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                Bitmap bmp = getBitmapFromAsset(getBaseContext(), "shark_sighted.jpg");
+                ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                final ByteArrayInputStream inputStream = new ByteArrayInputStream(outputStream.toByteArray());
+                final VisionServiceClient visionServiceClient = new VisionServiceRestClient(subscriptionKey, url);
+
+                AsyncTask<InputStream, String, String> visionTask = new AsyncTask<InputStream, String, String>() {
+                    ProgressDialog progressDialog = new ProgressDialog(find_meaning.this);
+
+                    @Override
+                    protected void onPreExecute() {
+                        progressDialog.show();
+                    }
+
+                    @Override
+                    protected void onPostExecute(String s) {
+                        progressDialog.dismiss();
+
+                        AnalysisResult result = new Gson().fromJson(s, AnalysisResult.class);
+                        StringBuilder result_text = new StringBuilder();
+
+                    }
+
+                    @Override
+                    protected String doInBackground(InputStream... inputStreams) {
+                        try{
+                            publishProgress("Recognizing...");
+                            String[] features = {"Description"};
+                            String[] details={};
+
+                            AnalysisResult result = visionServiceClient.analyzeImage(inputStreams[0], features, details);
+                            String jsonResult = new Gson().toJson(result);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (VisionServiceException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+
+
+
                 try {
-                    String xx = new executeDict().execute().get();
+                    String xx = new executeDict().execute(inputStream).get();
                 } catch (ExecutionException e) {
                     e.printStackTrace();
                     Log.i("dict", e.toString());
